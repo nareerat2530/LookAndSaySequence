e381fde477e9c623b7e94f2e3126a9d75a75abd5
nareerat2530 <nareerat.srisai@gmail.com>
2022-12-10 17:34:56 +0100

    Initial commit

---
+++ Array challenge/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.VisualBasic;
+
+class MainClass
+{
+    private static int ArrayChallenge(int num)
+    {
+        var numberString = num.ToString();
+        var counter = 1;
+        var result = "";
+        var numberToCompareWith = numberString[0];
+
+        for (var i = 1; i < numberString.Length; i++)
+        {
+            //compare char digit with number 
+                if (numberToCompareWith ==numberString[i])
+                {
+                    counter++;
+                }
+                else
+                {
+                    result += counter;
+                     result +=numberToCompareWith;
+                    counter = 1;
+                    numberToCompareWith = numberString[i];
+                }
+        }
+        result += counter;
+        result +=numberToCompareWith;
+
+        return Convert.ToInt32(result);
+    }
+
+  
+
+    static void Main()
+    {
+        
+
+        
+
+    
+         Console.WriteLine(ArrayChallenge(1121));
+    }
+}
